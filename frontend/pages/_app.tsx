9a34fb18275f9e718a16e993efdd4eab48befa42
18ARPITHA <155936673+18ARPITHA@users.noreply.github.com>
2026-08-10 00:08:18 +0530

    Remove NhostProvider and simplify ApolloProvider usage

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,18 +1,14 @@
 import { useMemo } from "react";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
-import { NhostProvider } from "@nhost/nhost-js/react"; // wraps auth state for hooks used below
-import { nhost } from "../lib/nhost";
 import { makeApolloClient } from "../lib/apollo";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = useMemo(() => makeApolloClient(), []);
   return (
-    <NhostProvider nhost={nhost}>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </NhostProvider>
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
   );
 }
